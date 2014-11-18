0c532907af0c0f928d528a6a18337c370830070c
root <root@mgospodinov.mgospodinov>
2014-11-18 17:01:26 -0500

    Created production build task. run with $grunt build-prod

--- swordsAndBooks/Gruntfile.js
+++ swordsAndBooks/Gruntfile.js
@@ -1,10 +1,33 @@
 //Gruntfile
 
 module.exports = function(grunt) {
-
-//Initializing the configuration object
+  var timestamp = grunt.template.today("yyyymmddhMMss");
+  //Initializing the configuration object
   grunt.initConfig({
     // Task configuration
+    copy: {
+      main: {
+        files: [
+          {expand : true, src : 'public/**', dest : '../builds/current/'},
+          {expand : true, src : 'app/models/**', dest : '../builds/current/'},
+          {expand : true, src : 'app/controllers/**', dest : '../builds/current/'},
+          {expand : true, src : 'app/views/**', dest : '../builds/current/'},
+          {expand : true, src : 'app/models/**', dest : '../builds/current/'},
+          {expand : true, src : 'package.json', filter:'isFile', dest : '../builds/current/'},
+          {expand : true, src : 'config/**', dest : '../builds/current/'},
+          {expand : true, src : 'servStart.sh' , dest : '../builds/current/'}
+        ]
+      }
+    },
+    compress: {
+      main: {
+        options: {
+          archive: '../builds/website_build_'+ timestamp +'.zip'
+        },
+        expand: true,
+        src: '../builds/current/'
+      }
+    },
     concat: {
         options: {
           separator: ';',
@@ -28,7 +51,6 @@ module.exports = function(grunt) {
               "./public/css/style_test.css":"./app/assets/stylesheets/frontend.less",
             }
         }
-        //TODO setup production task
     },
     uglify: {
       options: {
@@ -39,6 +61,42 @@ module.exports = function(grunt) {
           //'./public/assets/javascript/frontend.js': './public/assets/javascript/frontend.js',
         }
       },
+    },
+    imagemin: {
+      png: {
+        options: {
+          optimizationLevel: 7
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: '.',
+            src: ['app/assets/images/**/*.png'],
+            // Could also match cwd line above. i.e. project-directory/img/
+            dest: 'public/img/',
+            ext: '.png'
+          }
+        ]
+      },
+      jpg: {
+        options: {
+          progressive: true
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: '.',
+            src: ['app/assets/images/**/*.jpg'],
+            // Could also match cwd. i.e. project-directory/img/
+            dest: 'public/img/',
+            ext: '.jpg'
+          }
+        ]
+      }
     },
     watch: {
       js_frontend: {
@@ -62,15 +120,26 @@ module.exports = function(grunt) {
       }
     }
   });
-
+  
   // Plugin loading
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  //grunt.loadNpmTasks('grunt-contrib-clean');
   // Task definitions
+  grunt.registerTask('build-prod',[
+    'imagemin',
+    'less',
+    'concat:js_frontend',
+    'uglify:frontend',
+    'copy',
+    'compress'
+  ]);
+  //by default only watch less files for changes and rebuild css
   grunt.registerTask('default', ['watch']);
-
 };
 
